74cda8f8c3c2b1b49b3f7a4bae54206d707d5461
Mohak Bajaj <bmohak87@gmail.com>
2024-10-31 03:14:33 +0530

    refactor: Update DashboardLayout component to remove unused code and add overflow to the container

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,4 @@
 import Sidebar from "@/components/dashboard/sidebar";
-import { getAvatarURL } from "@/lib/utils";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import { config } from "@/lib/auth";
 import PageLayout from "@/components/dashboard/PageLayout";
 
 export default async function DashboardLayout({
@@ -10,17 +6,12 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(config);
-  const avatarURL = getAvatarURL(session?.user?.username ?? "");
-
   return (
     <div className="flex h-dvh overflow-hidden">
       <Sidebar />
       <main className="no-scrollbar mx-auto h-dvh max-w-xl flex-1">
         <PageLayout>{children}</PageLayout>
       </main>
-      {/* prefetch the avatar */}
-      <Link href={avatarURL} className="invisible hidden" />
     </div>
   );
 }
--- components/dashboard/PageLayout.tsx
+++ components/dashboard/PageLayout.tsx
@@ -2,6 +2,10 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
+import { getAvatarURL } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
+import { Icons } from "@/assets/Icons";
 
 export default function PageLayout({
   children,
@@ -11,6 +15,11 @@ export default function PageLayout({
   const pathname = usePathname();
   const router = useRouter();
 
+  // prefetch the avatar
+  const { data: session } = useSession();
+  const avatarURL = getAvatarURL(session?.user?.username ?? "");
+  router.prefetch(avatarURL);
+
   return (
     <div className="flex flex-col items-center">
       <motion.button
@@ -19,9 +28,15 @@ export default function PageLayout({
         className="mt-2 p-2 text-center font-[family-name:var(--font-poppins)] font-semibold capitalize"
         onClick={() => router.refresh()}
       >
-        {pathname === "/" ? "Home" : pathname.slice(1).replace(/-/g, " ")}
+        <span className="hidden sm:block">
+          {pathname === "/" ? "Home" : pathname.slice(1).replace(/-/g, " ")}
+        </span>
+        <span className="flex items-center gap-1 sm:hidden">
+          <Icons.logo className="size-6" />
+          <p className="translate-y-0.5">{siteConfig.name}</p>
+        </span>
       </motion.button>
-      <div className="min-h-[calc(100dvh-3rem)] w-full overflow-y-auto rounded-t-xl border-x border-t border-border">
+      <div className="min-h-[calc(100dvh-3rem)] w-full overflow-y-auto border-t border-border sm:rounded-t-xl sm:border-x">
         {children}
       </div>
     </div>
