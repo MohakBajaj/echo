9c675a645ea820051e1a908c9dff99cfb3af7f2f
Mohak Bajaj <bmohak87@gmail.com>
2024-11-09 15:16:05 +0530

    refactor: Update MediaItem component to add a dialog for media preview

--- components/dashboard/create-dialog/media-item.tsx
+++ components/dashboard/create-dialog/media-item.tsx
@@ -1,17 +1,26 @@
 import { X } from "lucide-react";
 import { motion } from "framer-motion";
-import { memo } from "react";
+import { memo, useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 const MediaWrapper = memo(
   ({
     children,
     onRemove,
+    onClick,
   }: {
     children: React.ReactNode;
     onRemove: () => void;
+    onClick: () => void;
   }) => (
     <motion.div
-      className="group relative"
+      className="group relative cursor-pointer"
       initial={{ opacity: 0, scale: 0.95 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.95 }}
@@ -21,11 +30,15 @@ const MediaWrapper = memo(
         damping: 20,
         duration: 0.2,
       }}
+      onClick={onClick}
     >
       {children}
       <motion.button
         className="absolute right-1 top-1 rounded-full bg-background/80 p-1.5 opacity-100 transition-opacity hover:bg-background md:opacity-0 md:group-hover:opacity-100"
-        onClick={onRemove}
+        onClick={(e) => {
+          e.stopPropagation();
+          onRemove();
+        }}
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         transition={{
@@ -78,11 +91,58 @@ export const MediaItem = memo(
     src: string;
     onRemove: () => void;
   }) => {
+    const [showDialog, setShowDialog] = useState(false);
+
     return (
-      <MediaWrapper onRemove={onRemove}>
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        {type === "image" ? <Image src={src} /> : <Video src={src} />}
-      </MediaWrapper>
+      <>
+        <MediaWrapper onRemove={onRemove} onClick={() => setShowDialog(true)}>
+          {type === "image" ? <Image src={src} /> : <Video src={src} />}
+        </MediaWrapper>
+
+        <Dialog open={showDialog} onOpenChange={setShowDialog}>
+          <DialogContent
+            className="flex min-h-screen min-w-[100vw] flex-col items-center justify-center border-none bg-transparent shadow-none backdrop-blur-xl ~p-4/14"
+            hideClose
+          >
+            <DialogTitle className="sr-only">Media Preview</DialogTitle>
+            <DialogDescription className="sr-only">
+              {type === "image" ? "Image Preview" : "Video Preview"}
+            </DialogDescription>
+            {type === "image" ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={src}
+                alt="image media"
+                className="h-auto max-h-[80vh] w-auto rounded-md object-contain"
+              />
+            ) : (
+              <video
+                src={src}
+                controls
+                controlsList="nodownload noremoteplayback"
+                playsInline
+                preload="metadata"
+                className="h-auto max-h-[80vh] w-auto rounded-md object-contain"
+                onContextMenu={(e) => e.preventDefault()}
+              />
+            )}
+            <DialogClose asChild>
+              <motion.button
+                className="absolute right-2 top-2 rounded-full border-2 border-background/80 bg-transparent p-1.5 opacity-100 transition-opacity hover:bg-background/10"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{
+                  type: "spring",
+                  bounce: 0.05,
+                  duration: 0.2,
+                }}
+              >
+                <X className="~size-8/10" />
+              </motion.button>
+            </DialogClose>
+          </DialogContent>
+        </Dialog>
+      </>
     );
   }
 );
